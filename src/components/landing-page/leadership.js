7dd9d9b943c5fcd7417b0d57ed3688d1bd42133d
Camilo Barraza <acordeonlwebmaster@gmail.com>
2019-12-05 15:19:05 -0500

    add leadership section

--- src/components/landing-page/leadership.js
+++ src/components/landing-page/leadership.js
@@ -2,28 +2,30 @@ import React, { Component } from 'react'
 import { SectionHeader } from "./sectionHeader"
 import { useStaticQuery, graphql } from "gatsby"
 
-const Leader = (props) => (<div className='wrapper horizontal'>
-  <img className='profile-img' src='mike.png' width="137px" height= "137px"></img>
+const Leader = ({profileImage, name, role, description}) => (<div className='wrapper horizontal'>
+  <img className='profile-img' src={profileImage.sourceUrl} width="137px" height= "137px"></img>
   <div className='leader-info'>
     <div className='name'>
-      Michael Frederick
+      {name.toUpperCase()}
     </div>  
     <div className='role'>
-      Chief Executive Officer
+      {role}
     </div>
     <div className='underline'> </div>
-    <div className='description'>
-      Michael has 17+ years of experience building and managing software with projects ranging from startup MVP development to managing complex systems for Fortune 500 companies. Michael is passionate about eliminating waste in the software development ecosystem, and he started Flatirons Development with the mission of improving the efficiency and quality of product development.
-    </div>
+    <div className='description' dangerouslySetInnerHTML={{__html:description}}></div>
   </div>
   <style jsx>{`
     .profile-img{
       border-radius:50%;
     }
     .leader-info{
-
+      margin-left:49px;
+    }
+    .wrapper{
+      margin-top:68px;
     }
     .name{
+      text-align:left;
       width: 211px;
       height: 19px;
       font-family: Montserrat;
@@ -33,16 +35,26 @@ const Leader = (props) => (<div className='wrapper horizontal'>
       font-style: normal;
       line-height: normal;
       letter-spacing: 1.9px;
-      color: var(--dark-indigo);
-
+      margin-bottom:10px;
+      color: #071638;
     }
     .role{
-
+      text-align:left;
+      font-family: Montserrat;
+      font-size: 32px;
+      font-weight: 600;
+      color: #071638;
     }
     .underline{
-      
+      width: 28px;
+      height: 1px;
+      border: solid 1.5px #d0a617;
+      margin-top:13px;
+      margin-left:1px;
     }
     .description{
+      text-align:left;
+      margin-top:27px;
       width: 719px;
       font-family: Montserrat;
       font-size: 18px;
@@ -52,22 +64,55 @@ const Leader = (props) => (<div className='wrapper horizontal'>
   `}</style>
 </div>)
 
-export const Leadership = (props) => (<div className='wrapper centered'>
-  <div >
-    <div className='section-header'>
-      <SectionHeader name='leadership' title='Our leadership team works directly with you'></SectionHeader>
-    </div>
+export const Leadership = () =>  {
+  let { fl: { pages: { nodes:[ { pageContent: { leadership } } ] } }  } = useStaticQuery(
+    graphql`
+    query Leadership {
+      fl{
+        pages(where:{title:"Landing"}){
+           nodes{
+            title
+            pageContent{
+              leadership{
+                title
+                leaders{
+                  name
+                  role
+                  description
+                  profileImage{
+                    sourceUrl
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  return (<div className='wrapper centered'>
     <div >
-      <Leader></Leader>
+      <div className='section-header'>
+        <SectionHeader name='leadership team' title={leadership.title}></SectionHeader>
+      </div>
+      <div className='leaders'>
+        {leadership.leaders.map((leader, index) => (<div key={index}>
+          <Leader {...leader} />
+        </div>))}
+      </div>
     </div>
-  </div>
-  <style jsx>{`
-    .section-header{
-      margin-top:89px;
-    }
-    .wrapper{
-      object-fit: contain;
-      background-image: linear-gradient(to bottom, #f6f9fc, #ffffff);
-    }
-  `}</style>
-</div>)
+    <style jsx>{`
+      .leaders{
+        margin-bottom:155px;
+      }
+      .section-header{
+        margin-top:89px;
+        margin-bottom:119px;
+      }
+      .wrapper{
+        object-fit: contain;
+        background-image: linear-gradient(to bottom, #f6f9fc, #ffffff);
+      }
+    `}</style>
+  </div>)
+}
