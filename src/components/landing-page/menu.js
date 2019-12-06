0aa2fff9b2785f3538258c5b21e8519575aa8578
Camilo Barraza <acordeonlwebmaster@gmail.com>
2019-12-06 14:52:13 -0500

    add responsiveness for top menu

--- src/components/landing-page/menu.js
+++ src/components/landing-page/menu.js
@@ -1,21 +1,32 @@
 import { useMenuItems } from "../../content-hooks/landing-page/useMenuItems"
 import logoImg from './assets/white-logo.svg'
+import mobileLogoImg from './assets/mobile-logo.png'
 import React  from "react"
  
 export const Menu = () => {
   let menuItems = useMenuItems('Landing Page')
   return (<div className='wrapper centered'>
     <div className='container space-between'>
-      <div className='logo-wrapper centered'>
+      <div className='logo-wrapper desktop-logo centered'>
         <img draggable="false" src={logoImg} alt='logo'></img>
       </div>
-      <div className='menu-wrapper centered'>
+      <div className='logo-wrapper mobile-logo centered'>
+        <img draggable="false" src={mobileLogoImg} alt='logo'></img>
+      </div>
+      <div className='menu-wrapper desktop-menu centered'>
         <div className='menu horizontal'>
           {menuItems.map((menuItem, index) => (<div key={index}>
             <div className='menu-item'>
               <a href={menuItem.node.url}> {menuItem.node.label} </a>
             </div>
           </div>))}
+        </div>
+      </div>
+      <div className='menu-wrapper mobile-menu centered'>
+        <div className='menu horizontal'>
+          <div className='menu-item'>
+            <a href={menuItems[menuItems.length-1].node.url}> {menuItems[menuItems.length-1].node.label} </a>
+          </div>
         </div>
       </div>
     </div>
@@ -28,6 +39,56 @@ export const Menu = () => {
       }
       img{
         margin:0;
+      }
+      @media all and (max-width: 1600px) {
+        .container{
+          width:97% !important;
+        }
+      }
+      
+      @media all and (max-width: 920px) {
+        .mobile-logo {
+          display:flex !important;
+        }
+        .desktop-logo{
+          display:none !important;
+        }
+        .desktop-menu{
+          margin-top:-2vw !important;
+        }
+      }
+      @media all and (max-width: 750px) {
+        .mobile-menu{
+          margin-top:-3vw;
+          display:flex !important;
+        }
+        .desktop-menu{
+          display:none !important;
+        }
+      }
+      @media all and (max-width: 500px) {
+        .container{
+          margin-top:12vw !important;
+        }
+        .mobile-logo img{
+          height:24px !important;
+        }
+      }
+      
+      .mobile-menu{
+        display:none;
+      }
+      .desktop-logo{
+        margin-left:15px;
+      }
+      .mobile-logo{
+        margin-left:15px;
+        margin-top:-10px;
+        display:none;
+      }
+      .mobile-logo img{
+        margin-left:1vw;
+        height:5vw;
       }
       .container{
         margin-top:50px;
